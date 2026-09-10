922374b284ade6b6ec8510dd65d9ba121f1fbfa4
daniyalraza71 <daniyalraza2410d@aptechgdn.net>
2026-09-11 00:14:23 +0500

    Remove .env.local from repository

--- .next/dev/static/chunks/_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/app_globals_0yg4wg8.css","static/chunks/node_modules_next_dist_20wefz_._.js","static/chunks/node_modules_@supabase_auth-js_dist_module_1vqrmkg._.js","static/chunks/node_modules_micromark-core-commonmark_dev_lib_2079wuz._.js","static/chunks/node_modules_refractor_lang_1o9hell._.js","static/chunks/node_modules_refractor_lib_1ew2_kc._.js","static/chunks/node_modules_pdfjs-dist_build_pdf_mjs_0ikqf4f._.js","static/chunks/node_modules_@supabase_1y2uk-1._.js","static/chunks/node_modules_0r58n5r._.js","static/chunks/_0u843ih._.js"],
-    source: "entry"
-});
